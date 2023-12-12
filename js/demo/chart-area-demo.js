0ccf72bec8f0c156f5cb2c931ce0ab823e66ae2c
ranjit485 <patilranjit485@gmail.com>
2023-12-11 13:39:00 -1200

    demo lgin javascript added

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -34,7 +34,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
-      label: "Earnings",
+      label: "Leaves",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 3, 5, 1, 2, 5, 4, 1, 2, 3, 4, 4],
     }],
   },
   options: {
