86137ce14ae58523293df60a74fe480b6dce5e13
RanjitPatil <patilranjit485@gmail.com>
2024-05-31 14:52:42 +0530

    update

--- databases/canigo_database.sql
+++ databases/canigo_database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2024 at 06:20 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: May 31, 2024 at 11:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,6 +45,23 @@ INSERT INTO `admin` (`AdminID`, `ProfilePhoto`, `FirstName`, `LastName`, `course
 (1, '', 'BE Tech', 'Admin', 'BE Tech', 'BETECHADMIN', 'BETECHADMIN'),
 (2, '', 'Diploma', 'Admin', 'Diploma', 'DIPLOMAADMIN', 'DIPLOMAADMIN'),
 (3, '', 'ITI', 'Admin', 'ITI', 'ITIADMIN', 'ITIADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `StudentID` int(11) DEFAULT NULL,
+  `ComplaintType` varchar(250) NOT NULL,
+  `DateTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `HODID` int(11) DEFAULT NULL,
+  `ByAdmin` varchar(50) DEFAULT NULL,
+  `ByHOD` varchar(90) NOT NULL,
+  `Discription` text DEFAULT NULL,
+  `ComplaintID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -91,10 +108,8 @@ CREATE TABLE `hods` (
 --
 
 INSERT INTO `hods` (`HODID`, `ProfilePhoto`, `FirstName`, `LastName`, `course`, `Department`, `Username`, `Password`) VALUES
-(5, '../profiles/hods/AAV POly CM.jpg', 'Kiran', 'Gaikwad', 'Diploma', 'Computer Science', 'kiran', 'kiran'),
-(6, '../profiles/hods/daksh-kapoor.png', 'Manoj', 'Yadav', 'Diploma', 'AI', 'manoj', 'manoj'),
-(7, '../profiles/hods/AAV POly CM.jpg', 'Aashish', 'Vankundare', 'Diploma', 'Computer Science', '1909910050', 'root'),
-(9, '../profiles/hods/daksh-kapoor.png', 'GANESH', 'PAWAR', 'Diploma', 'Computer Science', '0987654321', 'ganewsh');
+(1, '../profiles/hods/AAV POly CM.jpg', 'Atul', 'Gaikwad', 'Diploma', 'Computer Technology', 'DIPLOMACMHOD', 'DIPLOMACMHOD'),
+(2, '../profiles/hods/daksh-kapoor.png', 'Mahesh', 'Yadav', 'Diploma', 'AI', 'DIPLOMAAIHOD', 'DIPLOMAAIHOD');
 
 -- --------------------------------------------------------
 
@@ -117,17 +132,6 @@ CREATE TABLE `leaves` (
   `LeaveID` int(11) NOT NULL,
   `Status` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `leaves`
---
-
-INSERT INTO `leaves` (`StudentID`, `DateTime`, `TeacherID`, `HODID`, `LeaveType`, `StartDate`, `EndDate`, `ByGuard`, `Reason`, `TeacherApprovalStatus`, `HODApprovalStatus`, `LeaveID`, `Status`) VALUES
-(35, '2024-01-27 17:04:49', 9, 6, 'Sick Leave', '2024-01-23 17:04:00', '2024-01-24 17:04:00', NULL, 'Example new', 'Assigned', 'Assgined', 65, 'Pending'),
-(35, '2024-01-22 18:13:40', 9, 6, 'Other', '2024-01-22 18:13:00', '2024-01-23 18:13:00', NULL, 'Hello Bhaskar Harugade..........', 'Assigned', 'Assgined', 66, 'Pending'),
-(35, '2024-01-22 18:14:35', 9, 6, 'Other', '2024-01-22 18:14:00', '2024-01-23 18:14:00', NULL, 'Bhaskar Harugade....', 'Assigned', 'Assgined', 67, 'Pending'),
-(35, '2024-01-22 18:19:14', 9, 6, 'Other', '2024-01-22 18:19:00', '2024-01-23 18:19:00', NULL, '..........................', 'Assigned', 'Assgined', 68, 'Pending'),
-(35, '2024-01-23 10:34:01', 9, 6, 'Other', '2024-01-23 10:33:00', '2024-01-24 10:33:00', NULL, 'Hello myself bhaskar Harugade ', 'Assigned', 'Assgined', 69, 'Pending');
 
 -- --------------------------------------------------------
 
@@ -155,8 +159,40 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`StudentID`, `RollNo`, `ProfilePhoto`, `FirstName`, `LastName`, `course`, `Department`, `Class`, `StudentContactNo`, `ParentContactNo`, `Username`, `Password`) VALUES
-(34, 1, '../profiles/students/daksh-kapoor.png', 'Ranjit                               ', 'Patil                               ', 'Diploma', 'Computer Science', 'TY', '9766800965', '7499491002', '  1909910058', 'root'),
-(35, 2, '', 'Bhaskar', 'Haragude', 'Diploma', 'Computer Science', 'TY', '0987654321', '8830573512', 'bhaskar', 'bhaskar');
+(34, 1, '../profiles/students/daksh-kapoor.png', 'Ranjit ', 'Patil                                  ', 'Diploma', 'Computer Science', 'SY', '9766800965', '7499491002', '1909910057', 'root'),
+(35, 2, '../profiles/students/man2.png', 'Bhaskar', 'Haragude', 'Diploma', 'Computer Science', 'TY', '0987654321', '8830573512', 'bhaskar', 'bhaskar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_notc`
+--
+
+CREATE TABLE `students_notc` (
+  `StudentID` int(11) NOT NULL,
+  `RollNo` int(11) NOT NULL,
+  `ProfilePhoto` varchar(500) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `course` varchar(250) NOT NULL,
+  `Department` varchar(255) DEFAULT NULL,
+  `Class` varchar(255) DEFAULT NULL,
+  `StudentContactNo` varchar(15) DEFAULT NULL,
+  `ParentContactNo` varchar(15) DEFAULT NULL,
+  `Username` varchar(50) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students_notc`
+--
+
+INSERT INTO `students_notc` (`StudentID`, `RollNo`, `ProfilePhoto`, `FirstName`, `LastName`, `course`, `Department`, `Class`, `StudentContactNo`, `ParentContactNo`, `Username`, `Password`) VALUES
+(0, 11, '../profiles/students/man2.png', 'Ranjit', 'Patil', 'Diploma', '#', 'SY', '9766800965', '9766800965', '9766800965', '9766800965'),
+(0, 12, '../profiles/students/man2.png\r\n', 'Ranjit', 'Patil', 'Diploma', 'Computer Science', 'TY', '9766800965', '9766800965', '9766800965', '9766800965'),
+(1, 12, '../profiles/students/man2.png\r\n', 'Ranjit', 'Patil', 'Diploma', 'Computer Science', 'TY', '9766800965', '9766800965', '9766800965', '9766800965'),
+(0, 99, '../profiles/students/man2.png', 'Ranjit', 'Patil', 'BE', '#', 'TY', '9766800965', '9766800965', '9766800965', '9766800965'),
+(0, 2, 'profiles/students/man2.png', 'Ranjit', 'xx', 'BE', '#', 'SY', '9766800965', '7499491002', '9766800962', '9766800965');
 
 -- --------------------------------------------------------
 
@@ -182,9 +218,25 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`TeacherID`, `ProfilePhoto`, `FirstName`, `LastName`, `Class`, `course`, `Department`, `Position`, `Username`, `Password`) VALUES
-(6, '../profiles/teachers/daksh-kapoor.png', 'Mahesh', 'Pawar', 'SY', 'Diploma', 'Computer Science', 'Class Teacher', 'mahesh', 'mahesh'),
-(9, '../students\\profileScreenshot 2023-10-03 113643.png', 'Snehal', 'kinkar', 'TY', 'Diploma', 'AI', 'Class Teacher', '0987654321', 'snhal'),
-(16, '../profiles/teachers/AAV POly CM.jpg', 'Ranjit', 'Patil', 'TY', 'Diploma', 'Computer Science', 'Class Teacher', '1909910050', 'root');
+(6, '../profiles/teachers/daksh-kapoor.png', 'Mahesh k', 'Pawar', 'SY', 'Diploma', 'Computer Technology', 'Lab Assistant', 'mahesh', 'mahesh'),
+(9, '../students\\profileScreenshot 2023-10-03 113643.png', 'Snehal', 'kinkar', 'TY', 'Diploma', 'AI', 'Class Teacher', '0987654321', 'snhal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_complaints`
+--
+
+CREATE TABLE `teacher_complaints` (
+  `TeacherID` varchar(200) NOT NULL,
+  `ComplaintType` varchar(250) NOT NULL,
+  `DateTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `ByAdmin` varchar(200) NOT NULL,
+  `ByHOD` varchar(200) NOT NULL,
+  `Discription` text NOT NULL,
+  `ComplaintID` int(11) NOT NULL,
+  `HODID` int(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -195,6 +247,15 @@ INSERT INTO `teachers` (`TeacherID`, `ProfilePhoto`, `FirstName`, `LastName`, `C
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`AdminID`);
+
+--
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`ComplaintID`),
+  ADD UNIQUE KEY `ComplaintID` (`ComplaintID`),
+  ADD KEY `complaints_hodid_foreign` (`HODID`),
+  ADD KEY `complaints_studentid_foreign` (`StudentID`);
 
 --
 -- Indexes for table `hods`
@@ -227,6 +288,13 @@ ALTER TABLE `teachers`
   ADD UNIQUE KEY `teachers_username_unique` (`Username`);
 
 --
+-- Indexes for table `teacher_complaints`
+--
+ALTER TABLE `teacher_complaints`
+  ADD PRIMARY KEY (`ComplaintID`),
+  ADD KEY `teacher_complaints_hodid_foreign` (`HODID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -235,6 +303,12 @@ ALTER TABLE `teachers`
 --
 ALTER TABLE `admin`
   MODIFY `AdminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `ComplaintID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `hods`
@@ -246,7 +320,7 @@ ALTER TABLE `hods`
 -- AUTO_INCREMENT for table `leaves`
 --
 ALTER TABLE `leaves`
-  MODIFY `LeaveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `LeaveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT for table `students`
@@ -259,6 +333,12 @@ ALTER TABLE `students`
 --
 ALTER TABLE `teachers`
   MODIFY `TeacherID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `teacher_complaints`
+--
+ALTER TABLE `teacher_complaints`
+  MODIFY `ComplaintID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
